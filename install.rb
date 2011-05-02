d45e0c38afe20f18c952cbdd379f775dcc4bbbaa
Steven Webb <steven.daniel.webb@gmail.com>
2011-05-02 08:27:47 +0800

    Don't link to .gitignore and fixed indenting.

--- install.rb
+++ install.rb
@@ -15,6 +15,7 @@ dot_files = Dir.entries(path)
 dot_files.delete(".")
 dot_files.delete("..")
 dot_files.delete(".git")
+dot_files.delete(".gitignore")
 dot_files.delete("Rakefile")
 dot_files.delete("install.rb")
 dot_files.delete("backup")
@@ -22,11 +23,11 @@ dot_files.delete("README.markdown")
 
 Dir.mkdir(backup_path) unless File.directory?(backup_path)
 dot_files.each do |file|
-	old_file = File.join(home, "." + file)
-	if File.exist?(old_file)
-		p "Moving #{old_file} #{backup_path}"
-		FileUtils.mv(old_file, backup_path)
-	end
-	FileUtils.ln_s(File.join(path, file), old_file)
+  old_file = File.join(home, "." + file)
+  if File.exist?(old_file)
+    p "Moving #{old_file} #{backup_path}"
+    FileUtils.mv(old_file, backup_path)
+  end
+  FileUtils.ln_s(File.join(path, file), old_file)
 end
 
